0361ce77fa8ba997a33188599f66d23ae1ff9013
Julian Kalinowski <Julian.Kalinowski@dataport.de>
2024-09-18 08:27:59 +0200

    set jdk

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `kotlin-dsl`
 }
 
 repositories {
     mavenCentral()
+}
+
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17 // hardcode to default android studio embedded jdk version JavaVersion.toVersion(libs.versions.jvmTarget.get())
+    targetCompatibility = JavaVersion.VERSION_17 // hardcode to default android studio embedded jdk version  JavaVersion.toVersion(libs.versions.jvmTarget.get())
+}
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString() // hardcode to default android studio embedded jdk version libs.versions.jvmTarget.get()
 }
